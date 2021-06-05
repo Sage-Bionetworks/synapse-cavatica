bcb32dc376b2b61d37090800b292e67c4d1f751a
thomasyu888 <thomas.yu@sagebase.org>
2021-06-05 10:39:18 +0800

    Move

--- scripts/rnaseq_flow.py
+++ scripts/rnaseq_flow.py
@@ -1,1 +1,20 @@
+"""
+This is a a proof-of-concept flow for some RNA-seq data using this
+KF RNA-Seq workflow: https://github.com/kids-first/kf-rnaseq-workflow
+Link on CAVATICA: https://cavatica.sbgenomics.com/public/apps#cavatica/apps-publisher/kfdrc-rnaseq-workflow/
+
+Here are the high level steps:
+
+1. RNA-seq data files [FASTQ] and metadata [annotations/CSV] indexed in
+   Synapse, stored in S3
+1. Manual submission that couples synapseclient and CAVATICA API
+1. Semi-automated submission using Synapse Evaluation API or AWS Lambda
+1. Execution of processing workflow [CWL] in CAVATICA environment
+    - if there should be status updates back to Synapse
+      (e.g. 58% of processing is done)
+1. Return of results [BAMs, TSVs] to Synapse, or elsewhere.
+
+More details of this task can be found here:
+https://github.com/include-dcc/stwg-issue-tracking/issues/7
+"""
 import synapseclient
