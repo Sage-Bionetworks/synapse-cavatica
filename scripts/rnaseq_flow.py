a604da8de31af2b2cdbe6c675899a94b58b8c81b
thomasyu888 <thomas.yu@sagebase.org>
2021-08-31 06:36:07 -0700

    Split up monitoring and evaluating submission

--- scripts/rnaseq_flow.py
+++ scripts/rnaseq_flow.py
@@ -102,7 +102,6 @@ def read_json_submission(filepath):
       raise ValueError("Input must be a valid json file")
 
 
-
 def store_synid_to_cavatica(syn, sbg_api, input_json, cavatica_project_id):
    """Take any inputs that are synapse ids, download the entity,
    store them onto cavatica and update input json"""
@@ -122,27 +121,18 @@ def store_synid_to_cavatica(syn, sbg_api, input_json, cavatica_project_id):
    return input_json
 
 
-def evaluate_submissions():
+def evaluate_submissions(syn, api):
    """Workflow to evaluate RECEIVED submissions"""
-   # Setup Seven bridges API
-   # https://github.com/sbg/okAPI/blob/a6c0816235ae8742913950d38cc5f57b5ab6314e/Recipes/CGC/Setup_API_environment.ipynb
-   # Pull credential from ~/.sevenbridges/credentials
-   config_file = sbg.Config(profile='cavatica')
-   api = sbg.Api(config=config_file)
-
    # CAVATICA project name
    project_name = "Test"
    # Public CAVATIC app-rnaseq workflow
    app_name = "Kids First DRC RNAseq Workflow"
-
    project = get_or_create_project(api=api, project_name=project_name)
 
    # Copy an application to your CAVATICA project
    # https://github.com/sbg/okAPI/blob/d3bcdeca309534603ae715cf2646c5f65e89d98f/Recipes/CGC/apps_copyFromPublicApps.ipynb
    copied_app = copy_or_get_app(api=api, app_name=app_name, project=project)
-
-   # Add step to download some fastq files from Synapse
-   syn = synapseclient.login()
+   print(copied_app)
 
    # Get all submissions
    evaluation_queue_id = 9614883
@@ -153,25 +143,23 @@ def evaluate_submissions():
 
       workflow_input = read_json_submission(sub_obj.filePath)
       # TODO: this can use DRS ids.
+      # Add step to download some fastq files from Synapse
       inputs = store_synid_to_cavatica(syn=syn, sbg_api=api,
                                       input_json=workflow_input,
                                       cavatica_project_id=project.id)
 
-      print(copied_app)
-      # Create task_id output folder to potentially store task output
-      # files into synapse
-      synapse_project = "syn25920979"
-      synapse_task_folder = syn.store(
-         synapseclient.Folder(name=task_id, parent=synapse_project)
-      )
       # TODO: Missing rest call that automatically copies
       # reference files?
       # Name the cavatica task the submission id
       task = api.tasks.create(name=sub.id, project=project.id,
                               app=copied_app.id, inputs=inputs, run=True)
       task_id = task.id
-      task = api.tasks.get(task_id)
-
+      # Create task_id output folder to potentially store task output
+      # files into synapse
+      synapse_project = "syn25920979"
+      synapse_task_folder = syn.store(
+         synapseclient.Folder(name=task_id, parent=synapse_project)
+      )
       # Create dict for submission annotations
       if not sub_status.get("submissionAnnotations"):
          sub_status.submissionAnnotations = {}
@@ -182,44 +170,64 @@ def evaluate_submissions():
       sub_status.status = "EVALUATION_IN_PROGRESS"
       sub_status = syn.store(sub_status)
 
-      # Sleep the program when the task is running
-      while task.status == "RUNNING":
-         time.sleep(60)
-         print("task is running")
-         task = api.tasks.get(task_id)
 
+def monitor_submissions(syn, api):
+   """Monitoring evaluating submissions"""
+   evaluation_queue_id = 9614883
+   sub_bundles = syn.getSubmissionBundles(evaluation_queue_id,
+                                          status='EVALUATION_IN_PROGRESS')
+   for _, sub_status in sub_bundles:
+      task_id = sub_status.submissionAnnotations['task_id'][0]
+      synapse_task_folder = sub_status.submissionAnnotations['task_output'][0]
+      task = api.tasks.get(task_id)
       # Update final submission status
       if task.status == "INVALID":
          sub_status.status = "INVALID"
+      elif task.status in ["RUNNING", "QUEUED"]:
+         print("task is running")
+         # move forward to next in the loop
+         continue
       elif task.status == "COMPLETED":
          sub_status.status = "ACCEPTED"
+         # Store task outputs if the task is complete
+         task_outputs = task.outputs
+         # Download outputs except for bam files (large)
+         temp_dir = tempfile.TemporaryDirectory()
+         output_files = []
+         for _, output_value in task_outputs.items():
+            if output_value is not None:
+               if not output_value.name.endswith(".bam"):
+                  output_file = api.files.get(output_value.id)
+                  output_path = os.path.join(temp_dir.name, output_value.name)
+                  output_file.download(output_path)
+                  output_files.append(output_path)
+
+         # Unfortunately no recursive store function currently...
+         for output_file in output_files:
+            syn.store(
+               synapseclient.File(path=output_file, parent=synapse_task_folder)
+            )
+         temp_dir.cleanup()
       else:
          raise ValueError(f"status {task.status} not supported:")
       sub_status = syn.store(sub_status)
 
-      task_outputs = task.outputs
-      # Download outputs except for bam files (large)
-      temp_dir = tempfile.TemporaryDirectory()
-      output_files = []
-      for _, output_value in task_outputs.items():
-         if output_value is not None:
-            if not output_value.name.endswith(".bam"):
-               output_file = api.files.get(output_value.id)
-               output_path = os.path.join(temp_dir.name, output_value.name)
-               output_file.download(output_path)
-               output_files.append(output_path)
 
-      # Unfortunately no recursive store function currently...
-      for output_file in output_files:
-         syn.store(
-            synapseclient.File(path=output_file, parent=synapse_task_folder)
-         )
-      temp_dir.cleanup()
+def main():
+   """Invoke"""
+   # Setup Seven bridges API
+   # https://github.com/sbg/okAPI/blob/a6c0816235ae8742913950d38cc5f57b5ab6314e/Recipes/CGC/Setup_API_environment.ipynb
+   # Pull credential from ~/.sevenbridges/credentials
+   config_file = sbg.Config(profile='cavatica')
+   api = sbg.Api(config=config_file)
 
+   # Login to synapse
+   syn = synapseclient.login()
 
-def monitor_submissions():
-   pass
+   # Evaluate and monitor submissions
+   evaluate_submissions(syn, api)
+   monitor_submissions(syn, api)
 
 
 if __name__ == "__main__":
-   evaluate_submissions()
+   main()
