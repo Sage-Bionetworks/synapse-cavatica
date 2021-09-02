dea780466f2e98e2968872ddb5680f595d1d60b7
thomasyu888 <thomas.yu@sagebase.org>
2021-09-02 07:12:42 -0700

    Revise script to download everything for one SRA at a time

--- scripts/sra_fastq.py
+++ scripts/sra_fastq.py
@@ -21,37 +21,30 @@ def main():
     # this will download the .sra files to cwd as sra_id/...
     # (will create directory if not present)
     for sra_id in sra_numbers:
-        print(f"Currently downloading: {sra_id}")
+        print(f"Currently downloading: {sra_id}", flush=True)
         prefetch_cmd = ["prefetch", sra_id]
-        print(f"The command used was: {' '.join(prefetch_cmd)}")
+        print(f"The command used was: {' '.join(prefetch_cmd)}", flush=True)
         subprocess.check_call(prefetch_cmd)
         # Store SRA file into Synapse
         # syn.store(synapseclient.File(f"{sra_id}/{sra_id}.sra", parent="syn26140285"))
 
-    # this will extract the .sra files from above into a folder named 'fastq'
-    for sra_id in sra_numbers:
-        print(f"Generating fastq for: {sra_id}")
-        # fastq_dump_cmd = [
-        #     "fastq-dump", "--outdir", "fastq", "--gzip",
-        #     "--skip-technical", "--readids", "--read-filter", "pass",
-        #     "--dumpbase", "--split-3",
-        #     "--clip", f"{sra_id}/{sra_id}.sra"
-        # ]
+        print(f"Generating fastq for: {sra_id}", flush=True)
         fastq_dump_cmd = ['fasterq-dump', sra_id, '-O', 'fastq']
-        print(f"The command used was: {' '.join(fastq_dump_cmd)}")
+        print(f"The command used was: {' '.join(fastq_dump_cmd)}", flush=True)
 
-    # Add fastq file
-    fastq_files = os.listdir('fastq')
-    for fastq in fastq_files:
-        fastq_path = os.path.join('fastq', fastq)
-        gzip_cmd = ['gzip', fastq_path]
-        print(" ".join(gzip_cmd))
-        subprocess.check_call(gzip_cmd)
-        print("Storing file")
-        syn.store(
-            synapseclient.File(f"{fastq_path}.gz", parent="syn26140163")
-        )
-        os.remove(f"{fastq_path}.gz")
+        # Add fastq files
+        fastq_files = os.listdir('fastq')
+        for fastq in fastq_files:
+            fastq_path = os.path.join('fastq', fastq)
+            gzip_cmd = ['gzip', fastq_path]
+            print(" ".join(gzip_cmd), flush=True)
+            subprocess.check_call(gzip_cmd)
+            print("Storing file", flush=True)
+            syn.store(
+                synapseclient.File(f"{fastq_path}.gz", parent="syn26140163")
+            )
+            os.remove(f"{fastq_path}.gz")
+        os.remove(f"{sra_id}/{sra_id}.sra")
 
 
 if __name__ == "__main__":
